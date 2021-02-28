c09c58117806cf77550abeca85f21dcb94876270
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2021-02-28 11:38:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<img src="/assets/logo.png" width="400px" styles="margin: 0 auto;">
+
+## O Projeto
+O raffle consiste em um gerador de número aleatórios para sorteios, o usuário insere quantos números seu sorteio possui e o Raffle sorteará um número aletório.
+
+<img src="/assets/screenshots/landing.png" width="400px">
+
+<img src="/assets/screenshots/result.png" width="400px">
+
+## Rodando Localmente
+
+Abra o arquivo <strong>index.html</strong> no seu navegador
---
+++ script.js
@@ -0,0 +1,31 @@
+function raffle(){
+    let inputValue = document.getElementById("number").value 
+    let raffleOne = Math.ceil(Math.random() * inputValue)
+    let modal = document.getElementById("modal")
+
+    if (modal.className === "modalOpen"){
+        showResult(raffleOne)
+    }
+    else{
+        openModal()
+        showResult(raffleOne)
+    }
+}
+
+function openModal(){
+    let modal = document.getElementById("modal")
+    modal.className = "modalOpen"
+}
+
+function showResult(result){
+    let resultElement = document.getElementById("result")
+    resultElement.innerText = "..."
+    setTimeout(()=>{
+        resultElement.innerText = result
+    }, 300)
+}
+
+function closeModal(){
+    let modal = document.getElementById("modal")
+    modal.className = "modalClosed"
+}
